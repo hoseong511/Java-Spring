86172ad6a82b5506f439a238ecb7e28eebc656c9
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-04-25 17:54:23 +0900

    update variableTest1

---
+++ src/ch06/VariableTest.java
@@ -0,0 +1,10 @@
+package ch06;
+
+public class VariableTest {
+    public static void main(String[] args) {
+        byte bnum = -128; // -128 ~ 127
+        System.out.println(bnum);
+        long lNum = 12345678900L; // long 표현
+        System.out.println(lNum);
+    }
+}
